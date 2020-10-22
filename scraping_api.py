5ecd8003af0dbcf894af9c61100c0131a70bae68
Aman Mishra <34169960+amanaation@users.noreply.github.com>
2020-10-23 02:15:39 +0530

    Update scraping_api.py

--- scraping_api.py
+++ scraping_api.py
@@ -1,21 +1,15 @@
 from flask import Flask, render_template, request
-from scrap_google_insights import Articles
+from scrap_google_insights import Products
 
 app = Flask(__name__)
-
-
-@app.route("/get", methods=['POST', 'GET'])
-def home():
-    return render_template("index.html")
-
 
 @app.route("/post", methods=['GET'])
 def process():
     sent_data = {}
     for key, value in request.args.items():
         sent_data[key] = str(value)
-    art = Articles()
-    art.main(sent_data)
+    prod = Products()
+    prod.main(sent_data)
     return "<h1> <center> Scraped the Required Data</center></h1>"
 
 
